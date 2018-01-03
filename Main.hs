c439674e8887b7f89b8d8e560342d446f813e38e
Dave Compton <davecompton7@gmail.com>
2018-01-03 14:36:59 +0000

    adding in Events; linted

--- Main.hs
+++ Main.hs
@@ -5,7 +5,7 @@ import Reflex.Dom
 import Data.Text (Text, pack) 
 import Data.Map (Map, fromList)
 import Data.Time.Clock (UTCTime, utctDay, getCurrentTime)
-import Data.Time.Calendar (toGregorian)
+import Data.Time.Calendar (Day, toGregorian)
 import Control.Monad.Trans (liftIO)
 
 type Point = (Float,Float)
@@ -24,30 +24,21 @@ toPoint angle = (cos angle,sin angle)
 
 finiteTicks :: MonadWidget t m => UTCTime ->Int -> m (Event t TickInfo)
 finiteTicks now limit = do
-  eTick :: Event t TickInfo <- tickLossy 0.01 now
-
-  dCount :: Dynamic t Int <- count eTick
-  dLimit :: Dynamic t Bool <- holdUniqDyn $ fmap (limit<) dCount 
-
-  let eLimit :: Event t Bool
-      eLimit = updated dLimit
-      
-      eCountPastLimit :: Event t Bool
-      eCountPastLimit = ffilter id eLimit
-
-      neverAfterLimit :: Event t (Event t TickInfo)
-      neverAfterLimit = never <$ eCountPastLimit
-
-  switchPromptly eTick neverAfterLimit
+  eTick <- tickLossy 0.01 now
+  dCount <- count eTick
+  dLimit <- holdUniqDyn $ fmap (limit<) dCount 
+  switchPromptly eTick (never <$ updated dLimit)
 
 addPoint :: Point -> Point -> Point
 addPoint (p0x, p0y) (p1x, p1y) = (p0x + p1x, p0y + p1y)
 
-sumOfTheDay :: MonadWidget t m => (Int, Int, Int) -> m ()
+newtype ChangeDate = DateChange Int
+
+sumOfTheDay :: MonadWidget t m => (Int, Int, Int) -> m (Event t ChangeDate)
 sumOfTheDay (y,m,d) = do
   now <- liftIO getCurrentTime 
 
-  let segmentCount = lcm (lcm m d) $ y
+  let segmentCount = lcm (lcm m d) y
       ymdAngle = toAngle (y,m,d)
       vectors = fmap (toPoint.ymdAngle) [0..segmentCount] 
       points = scanl addPoint (0.0,0.0) vectors
@@ -83,24 +74,33 @@ sumOfTheDay (y,m,d) = do
   let boardAttrs = 
         fromList [ ("width" , pack $ show 400)
                  , ("height", pack $ show 400)
-                 , ("viewBox", pack $ show (xMin) ++ " " ++ show (yMin) ++ " " ++ show width ++ " " ++ show height)
+                 , ("viewBox", pack $ show xMin ++ " " ++ show yMin ++ " " ++ show width ++ " " ++ show height)
                  ]
-  el "h2" $ do
-      button "<<"
-      text $ pack (show m ++ "/" ++ show d ++ "/" ++ show y)
-      button ">>"
+  ev <- el "h2" $ do
+            leftButton <- button "<<"
+            text $ pack (show m ++ "/" ++ show d ++ "/" ++ show y)
+            rightButton <- button ">>"
+
+            let prevEvent =  DateChange (-1) <$ leftButton 
+                nextEvent = DateChange 1 <$ rightButton 
+            return (leftmost [prevEvent, nextEvent])
+
   elSvgns "svg" (constDyn boardAttrs) (listHoldWithKey mempty eLineMap $ showLine (width/200.0))
-  return ()
+  return ev
 
-
-main = mainWidget $ do 
-  now <- liftIO getCurrentTime 
-  let uDay = utctDay now
-      (y0, m, d) = toGregorian uDay
+sumOfDayWidget :: MonadWidget t m => Day -> m (Event t ChangeDate)
+sumOfDayWidget uDay = do
+  let (y0, m, d) = toGregorian uDay
       y = fromIntegral y0 `mod` 100
   elAttr "div" (fromList [("style", "text-align:center")]) $ do
     el "h1" $ text "Exponential sum of the day"
     sumOfTheDay (y,m,d)
+
+
+main = mainWidget $ do 
+  now <- liftIO getCurrentTime 
+  _ <- sumOfDayWidget $ utctDay now
+  return ()
 
 -- The svg attributes needed to display a line segment.
 lineAttrs :: Float -> Segment -> Map Text Text
@@ -110,7 +110,7 @@ lineAttrs width ((x1,y1), (x2,y2)) =
            , ( "x2",    pack $ show x2)
            , ( "y2",    pack $ show y2)
            , ( "style", "stroke:midnightblue")
-           , ( "stroke-width", pack $ show $ width)
+           , ( "stroke-width", pack $ show width)
            ]    
 
 -- Use svg to display a line segment.
